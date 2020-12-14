48eb9c3c0cc8867459ced2f096ea597ff9720ab1
AdiatsaPutra <santikosetyo77@gmail.com>
2020-12-14 19:54:15 +0700

    Addes Register dan Login

---
+++ ClientReservasi_073/Login.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientReservasi_073
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+
+            string kategori = service.Login(username, password);
+
+            if(kategori == "Admin")
+            {
+                Register fm = new Register();
+                this.Hide();
+                fm.Show();
+            }
+            else if (kategori == "Resepsionis")
+            {
+                Form1 fm = new Form1();
+                this.Hide();
+                fm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Username dan Password Invalid, Silakan hubungi admin.");
+                textBoxUsername.Clear();
+                textBoxPassword.Clear();
+            }
+
+        }
+    }
+}
